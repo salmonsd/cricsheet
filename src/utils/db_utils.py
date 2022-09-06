4d65c0aecc09ac9ae75165406f08ffd6705a581b
salmonsd <22984014+salmonsd@users.noreply.github.com>
2022-09-06 17:01:00 -0500

    main: update db_utils.py add docstring, exception handling

--- src/utils/db_utils.py
+++ src/utils/db_utils.py
@@ -1,6 +1,19 @@
 import sqlite3
 
-DB_NAME = "./db/odi_cricsheet.db"
+DB_FILE = "./db/odi_cricsheet.db"
 
-def get_db_connection(db: str = DB_NAME):
-    return sqlite3.connect(db)
+
+def get_db_connection(db_file: str = DB_FILE):
+    """
+    create a database connection to the SQLite
+    database specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except:
+        print("ERROR: Unable to connect to sqlite")
+
+    return conn
