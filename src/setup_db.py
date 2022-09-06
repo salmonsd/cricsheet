dbb4634be555a07698a09f3e95b35e312bf79ea4
salmonsd <22984014+salmonsd@users.noreply.github.com>
2022-09-06 17:01:56 -0500

    main: update setup_db.py add logging and exception handling

--- src/setup_db.py
+++ src/setup_db.py
@@ -1,20 +1,40 @@
+import sys
+import logging
+
 from utils.config import TABLE_CONFIG
 from utils.db_utils import get_db_connection
+from utils.logger_utils import get_logger
 
-import logging
+
+# setup logging
+py_file = __file__.split("/")[-1]
+logger = get_logger(name=f"{py_file}:{__name__}")
+
 
 def setup_db():
     """
-    Gets a db connectios and then iterates 
+    Gets a db connection and then iterates
     through table DDLs to create db tables
     """
-    con = get_db_connection()
+    logger.info("Getting DB Connection")
+    conn = get_db_connection()
 
-    with con:
+    try:
+        with conn:
+            logger.info(f"Processing {len(TABLE_CONFIG)} tables for setup")
+            for table, ddl in TABLE_CONFIG.items():
+                logger.info(f"Creating table - {table}")
+                conn.execute(ddl)
 
-        for table, ddl in TABLE_CONFIG.items():
-            logging.info(f"{__name__}: creating table - {table}")
-            con.execute(ddl)
+            logger.info("Done setting up!")
+
+    except Exception as e:
+        logger.error(e)
+
+    finally:
+        logger.info("Closing DB Connection")
+        conn.close()
+
 
 if __name__ == "__main__":
     setup_db()
